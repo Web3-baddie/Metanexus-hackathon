e12ebf46aa5c17ac260c03e2ce3968a7e22bd5b5
kiminza <165162498+Web3-baddie@users.noreply.github.com>
2024-11-09 01:26:22 +0300

    Update and rename main.rs to betting_contract.rs

--- contracts/src/main.rs
+++
@@ -1,8 +0,0 @@
-#![cfg_attr(not(any(feature = "export-abi", test)), no_main)]
-#![cfg_attr(not(any(feature = "export-abi", test)), no_std)]
-
-#[cfg(feature = "export-abi")]
-fn main() {
-  chainsweep::main();
-}
-
